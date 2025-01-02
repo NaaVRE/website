7eb330eeb094df0825edb92ffaef779d52fa0d5f
KoenGreuell <koengreuell@gmail.com>
2025-01-02 14:41:10 +0100

    #271 Add first role page, refactor role text
    Move description of community supporter to separate file to allow text reuse.

---
+++ docs/readiness_levels/Text_templates.js
@@ -0,0 +1,31 @@
+const L0CommunitySupporter = () => {
+  return (
+    'Reaches out to domain scientists who have the potential of becoming golden users.'
+  );
+};
+
+const L1CommunitySupporter = () => {
+  return (
+    'Receives a demonstration of the virtual lab to gain insight in the virtual lab and potential use cases.'
+  );
+};
+
+const L2CommunitySupporter = () => {
+  return (
+    'Finds potential silver users to take part in the trainings in the next phase.'
+  );
+};
+
+const L3CommunitySupporter = () => {
+  return (
+    'Invites silver users to the virtual lab.'
+  );
+};
+
+const L4CommunitySupporter = () => {
+  return (
+    'Identifies any potential new golden users from the community using the virtual lab.'
+  );
+};
+
+export { L0CommunitySupporter, L1CommunitySupporter, L2CommunitySupporter, L3CommunitySupporter, L4CommunitySupporter };
