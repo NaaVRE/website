8a66bc6eec618529c3857a51d1d49dfdada2f864
KoenGreuell <koengreuell@gmail.com>
2025-01-02 16:14:17 +0100

    #271 Add role pages, refactor responsibilities

--- docs/readiness_levels/Responsibilities.js
+++ docs/readiness_levels/Responsibilities.js
@@ -1,12 +1,41 @@
-// GoldenUser
-export const L0GoldenUser = () => 'Have a use case that will serve as the first application of the virtual lab. Recruit a core development team.';
-export const L1GoldenUser = () => 'Guide the development of the lab from the scientific viewpoint.';
-export const L2GoldenUser = () => 'Use the virtual lab to do their research. Publish a paper in the ecosystem domain.';
-export const L3GoldenUser = () => 'The golden users who have used the virtual lab for their own investigations, will appoint or approve a principal investogator for the virtual lab.';
-
-// CommunitySupporter
 export const L0CommunitySupporter = () => 'Reaches out to domain scientists who have the potential of becoming golden users.';
 export const L1CommunitySupporter = () => 'Receives a demonstration of the virtual lab to gain insight in the virtual lab and potential use cases.';
 export const L2CommunitySupporter = () => 'Finds potential silver users to take part in the trainings in the next phase.';
 export const L3CommunitySupporter = () => 'Invites silver users to the virtual lab.';
 export const L4CommunitySupporter = () => 'Identifies any potential new golden users from the community using the virtual lab.';
+
+export const L0GoldenUser = () => 'Have a use case that will serve as the first application of the virtual lab. Recruit a core development team.';
+export const L1GoldenUser = () => 'Guide the development of the lab from the scientific viewpoint.';
+export const L2GoldenUser = () => 'Use the virtual lab to do their research. Publish a paper in the ecosystem domain.';
+export const L3GoldenUser = () => 'The golden users who have used the virtual lab for their own investigations, will appoint or approve a principal investogator for the virtual lab.';
+
+export const L0Coordinator = () => 'The virtual lab gets a coordinator assigned. Consults domain and technical experts to assess the whether running the virtual lab in NaaVRE is realistic. The coordinator ensures the golden users are onboarded onto NaaVRE and the readiness level framework.';
+export const L1Coordinator = () => 'Coordinates the co-development between the golden users, core developers and DevOps engineers.';
+export const L2Coordinator = () => 'Ensures the community supporter is involved in the virtual lab and supports solving any issues arising.';
+export const L3Coordinator = () => 'Monitors the virtual lab and any problems arising during training. Checks whether the needed principal investigator and user community emerges during this phase that provides a use case for maturing the virtual lab further.';
+export const L4Coordinator = () => 'Ensures the VRE keeps supporting the virtual lab.';
+
+export const L0DevOps = () => 'Establish if NaaVRE can support to use case. Create an empty new virtual lab.';
+export const L1DevOps = () => 'Support the creation of the new virtual lab by giving the core development team advice and changing the virtual research environment where necessary. Advice on the right infrastructure for the virtual lab.';
+export const L2DevOps = () => 'Support the core developers and golden user by providing advise and changing the virtual research environment where necessary.';
+
+export const L1CoreDeveloper = () => 'One or multiple core developers create a new virtual lab in NaaVRE.';
+export const L2CoreDeveloper = () => 'Provide support to the golden users.';
+
+export const L1Reviewer = () => 'Provides feedback on the user-friendliness, maintainability, and robustness of the source code and other assets. We recommend starting with reviews early on in the development process, as this may contribute to a clear architecture, and good code quality.';
+
+export const L1InfrastructureSupporter = () => 'Advises and provides the infrastructure for the virtual lab.';
+export const L2InfrastructureSupporter = () => 'Prepares the infrastructure for the trainings.';
+export const L3InfrastructureSupporter = () => 'Ensures the infrastructure can support the training sessions.';
+export const L4InfrastructureSupporter = () => 'Monitors, provides, and adjusts infrastructure for the virtual lab.';
+
+export const L2Trainer = () => 'Tries out the virtual lab and makes suggestions. Helps the golden users identify issues arising when others start using the virtual lab.';
+export const L3Trainer = () => 'Gives trainings to users. Can point out what the user can change and can not change in the lab.';
+
+export const L2VREResearcher = () => 'Potentially publishes a technical paper in addition to the domain paper.';
+
+export const L3PrincipalInvestigator = () => 'Gathers user feedback and determines how to facilitate other users in the virtual lab.';
+export const L4PrincipalInvestigator = () => 'Coordinates the further development of the virtual lab with a scientific vision.';
+
+export const L3UserSupporter = () => 'Answers any technical questions arising from the workshop use which the trainer can not answer.';
+export const L4UserSupporter = () => 'Is the primary point of contact in case any problems arise with the virtual lab.';
