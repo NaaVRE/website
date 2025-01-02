05e81fd18329e9109f75d1bb12cab1824d122543
KoenGreuell <koengreuell@gmail.com>
2025-01-02 15:40:10 +0100

    #Refactor Responsibilities/ Text templates

---
+++ docs/readiness_levels/Responsibilities.js
@@ -0,0 +1,12 @@
+// GoldenUser
+export const L0GoldenUser = () => 'Have a use case that will serve as the first application of the virtual lab. Recruit a core development team.';
+export const L1GoldenUser = () => 'Guide the development of the lab from the scientific viewpoint.';
+export const L2GoldenUser = () => 'Use the virtual lab to do their research. Publish a paper in the ecosystem domain.';
+export const L3GoldenUser = () => 'The golden users who have used the virtual lab for their own investigations, will appoint or approve a principal investogator for the virtual lab.';
+
+// CommunitySupporter
+export const L0CommunitySupporter = () => 'Reaches out to domain scientists who have the potential of becoming golden users.';
+export const L1CommunitySupporter = () => 'Receives a demonstration of the virtual lab to gain insight in the virtual lab and potential use cases.';
+export const L2CommunitySupporter = () => 'Finds potential silver users to take part in the trainings in the next phase.';
+export const L3CommunitySupporter = () => 'Invites silver users to the virtual lab.';
+export const L4CommunitySupporter = () => 'Identifies any potential new golden users from the community using the virtual lab.';
